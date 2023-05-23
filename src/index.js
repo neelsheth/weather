ebd72b8efea239e4d5c4617b4e2e77d0e68aa865
Neel Sheth <108276436+neelsheth@users.noreply.github.com>
2023-05-23 22:30:52 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import Snowfall from 'react-snowfall';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <Snowfall color={"white"} radius={[1.0, 5.0]} />
     <Header></Header>
     <Home></Home>
   </React.StrictMode>
